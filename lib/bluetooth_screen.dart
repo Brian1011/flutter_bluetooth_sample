c5e2404044aff67d2f2cb0706b63ab8ec11f4227
Brian Mutinda <mutindabrian26@gmail.com>
2022-06-21 16:56:11 +0300

    add bluetooth screen file

---
+++ lib/bluetooth_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BlueToothScreen extends StatefulWidget {
+  const BlueToothScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BlueToothScreen> createState() => _BlueToothScreenState();
+}
+
+class _BlueToothScreenState extends State<BlueToothScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
